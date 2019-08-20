52a27b809b5ae194476b6b4476db4a3c930a1613
Erica Murdock-Waters <ericahpa@gmail.com>
2019-08-20 04:12:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const multiply = var1 * var2
 let random = Math.random()
 
 //mod generates a remainder that is equal to 4
-
+const peach = 5
+let mod = 9 % peach
 
 // max will return 20 as the highest number in the set
 const max = Math.max(3, 17, 9, 12, 20, 0)
